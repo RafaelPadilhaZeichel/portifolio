65c183dbc17c5df045ea9fccf05aaa69ff46c9aa
Rafael Padilha <mexico@xgd3l.onmicrosoft.com>
2026-01-03 20:25:44 -0300

    Atualizando o portfólio

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: Tem que ter a barra no começo e no fim!
-  base: "/meu-portifolio/", 
+  // CORREÇÃO: O nome aqui deve ser igual ao do link do GitHub
+  base: "/portifolio/", 
 })
